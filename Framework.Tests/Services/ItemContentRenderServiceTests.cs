536e0c3e55cbbffc17b4b3948b61302ef69e6084
Franz Spitaler <spitefr@gmail.com>
2023-10-31 16:20:41 +0100

    First Framework Tests implemented.

---
+++ Framework.Tests/Services/ItemContentRenderServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Framework.Services.Tests
+{
+    [TestClass()]
+    public class ItemContentRenderServiceTests
+    {
+        [TestMethod()]
+        public void RenderContentTest_Ok()
+        {
+            var contentString = "A simple ToDo item";
+            var content = new ItemContentRenderService().RenderContent(contentString);
+            var contentNoOuterDiv = content.Replace("<div>", string.Empty).Replace("</div>", string.Empty);
+
+            Assert.IsNotNull(content);
+            Assert.AreEqual(contentString, contentNoOuterDiv);
+        }
+
+        [TestMethod()]
+        public void RenderContentTest_Replaced_Url()
+        {
+            var contentString = "A simple ToDo item with a link: www.google.com";
+            var content = new ItemContentRenderService().RenderContent(contentString);
+            var contentNoOuterDiv = content.Replace("<div>", string.Empty).Replace("</div>", string.Empty);
+
+            Assert.IsNotNull(contentNoOuterDiv);
+            Assert.AreNotSame(contentString, contentNoOuterDiv);
+            Assert.IsTrue(contentNoOuterDiv.Length > contentString.Length);
+        }
+
+        [TestMethod()]
+        public void RenderContentTest_Replaced_Email()
+        {
+            var contentString = "A simple ToDo item with an email-address: admin@google.com";
+            var content = new ItemContentRenderService().RenderContent(contentString);
+            var contentNoOuterDiv = content.Replace("<div>", string.Empty).Replace("</div>", string.Empty);
+
+            Assert.IsNotNull(contentNoOuterDiv);
+            Assert.AreNotSame(contentString, contentNoOuterDiv);
+            Assert.IsTrue(contentNoOuterDiv.Length > contentString.Length);
+        }
+    }
+}
---
+++ Framework.Tests/Services/ToastNotificationServiceTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Framework.Services.Tests
+{
+    [TestClass()]
+    public class ToastNotificationServiceTests
+    {
+        [TestMethod()]
+        public void SendNotificationTest_NoToast_Ok()
+        {
+            var service = new ToastNotificationService();
+
+            Assert.IsTrue(!service.DisplayedToasts.Any());
+        }
+
+        [TestMethod()]
+        public void SendNotificationTest_OneToast_Ok()
+        {
+            var service = new ToastNotificationService();
+            service.SendNotification("Test", "Test-Message", Base.MessageType.Warning);
+
+            Assert.IsTrue(service.DisplayedToasts.Any());
+        }
+
+        [TestMethod()]
+        public void SendNotificationTest_ThreeToast_Ok()
+        {
+            var service = new ToastNotificationService();
+            service.SendNotification("Test", "Test-Message #1", Base.MessageType.Info, 0);
+            service.SendNotification("Test", "Test-Message #2", Base.MessageType.Warning, 0);
+            service.SendNotification("Test", "Test-Message #3", Base.MessageType.Error, 0);
+
+            Assert.IsTrue(service.DisplayedToasts.Any());
+            Assert.IsTrue(service.DisplayedToasts.Count == 3);
+        }
+
+        [TestMethod()]
+        public async Task SendNotificationTest_ThreeToast_Then_Removed_Ok()
+        {
+            var service = new ToastNotificationService();
+            service.SendNotification("Test", "Test-Message #1", Base.MessageType.Info, 10);
+            service.SendNotification("Test", "Test-Message #2", Base.MessageType.Warning, 10);
+            service.SendNotification("Test", "Test-Message #3", Base.MessageType.Error, 10);
+
+            Assert.IsTrue(service.DisplayedToasts.Any());
+            Assert.IsTrue(service.DisplayedToasts.Count == 3);
+
+            await Task.Delay(100);
+
+            Assert.IsTrue(!service.DisplayedToasts.Any());
+        }
+    }
+}
