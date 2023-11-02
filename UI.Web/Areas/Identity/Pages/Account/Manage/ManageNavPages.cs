0335cde313c581fadd381764307180c4265398a9
Franz Spitaler <spitefr@gmail.com>
2023-11-02 22:12:27 +0100

    Added changePassword and enableAuthenticator pages.

--- UI.Web/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ UI.Web/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -68,8 +68,9 @@ namespace  UI.Web.Areas.Identity.Pages.Account.Manage
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
-
+        
         public static string SettingsNavClass(ViewContext viewContext) => PageNavClass(viewContext, Settings);
+
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
