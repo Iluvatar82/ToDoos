9766c2f0cb861141d07245258c1cd5eddc0fb3e1
Franz Spitaler <spitefr@gmail.com>
2023-11-09 20:58:43 +0100

    Warnings reduced.

--- Framework.Tests/Services/ToastNotificationServiceTests.cs
+++ Framework.Tests/Services/ToastNotificationServiceTests.cs
@@ -14,21 +14,21 @@ namespace Framework.Services.Tests
         }
 
         [TestMethod()]
-        public void SendNotificationTest_OneToast_Ok()
+        public async Task SendNotificationTest_OneToast_Ok()
         {
             var service = new ToastNotificationService();
-            service.SendAsync("Test", "Test-Message", Base.MessageType.Warning);
+            await service.SendAsync("Test", "Test-Message", Base.MessageType.Warning);
 
             Assert.IsTrue(service.DisplayedToasts.Any());
         }
 
         [TestMethod()]
-        public void SendNotificationTest_ThreeToast_Ok()
+        public async Task SendNotificationTest_ThreeToast_Ok()
         {
             var service = new ToastNotificationService();
-            service.SendAsync("Test", "Test-Message #1", Base.MessageType.Info, 0);
-            service.SendAsync("Test", "Test-Message #2", Base.MessageType.Warning, 0);
-            service.SendAsync("Test", "Test-Message #3", Base.MessageType.Error, 0);
+            await service.SendAsync("Test", "Test-Message #1", Base.MessageType.Info, 0);
+            await service.SendAsync("Test", "Test-Message #2", Base.MessageType.Warning, 0);
+            await service.SendAsync("Test", "Test-Message #3", Base.MessageType.Error, 0);
 
             Assert.IsTrue(service.DisplayedToasts.Any());
             Assert.IsTrue(service.DisplayedToasts.Count == 3);
@@ -38,9 +38,9 @@ namespace Framework.Services.Tests
         public async Task SendNotificationTest_ThreeToast_Then_Removed_Ok()
         {
             var service = new ToastNotificationService();
-            service.SendAsync("Test", "Test-Message #1", Base.MessageType.Info, 10);
-            service.SendAsync("Test", "Test-Message #2", Base.MessageType.Warning, 10);
-            service.SendAsync("Test", "Test-Message #3", Base.MessageType.Error, 10);
+            await service.SendAsync("Test", "Test-Message #1", Base.MessageType.Info, 10);
+            await service.SendAsync("Test", "Test-Message #2", Base.MessageType.Warning, 10);
+            await service.SendAsync("Test", "Test-Message #3", Base.MessageType.Error, 10);
 
             Assert.IsTrue(service.DisplayedToasts.Any());
             Assert.IsTrue(service.DisplayedToasts.Count == 3);
